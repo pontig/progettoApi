8df0aba18e6ba5e40b2ed5cb72b81a4c68426fa3
Elia Pontiggia <pontiggiaelia@gmail.com>
2024-02-15 22:45:27 +0100

    debug files fatti più meglio

--- run_tests.sh
+++ run_tests.sh
@@ -12,6 +12,7 @@ compare_files() {
 if [ $? -ne 0 ]; then
     echo "Errore di compilazione"
     exit 1
+else echo "Compilazione avvenuta con successo"
 fi
 
 for i in {1..112}
@@ -19,9 +20,11 @@ do
     inputFile="open_tests/open_$i.txt"
     expectedOutputFile="open_tests/open_$i.output.txt"
     echo "Testing $i..."
+    start_time=$(date +%s.%N)
     ./a.out < $inputFile > output.txt
-    compare_files output.txt $expectedOutputFile $i
+    end_time=$(date +%s.%N)
+    elapsed_time=$(echo "$end_time - $start_time" | bc)
+    compare_files output.txt $expectedOutputFile $i $elapsed_time
     rm output.txt
-    #echo "-----------------------------------"
 done
 
